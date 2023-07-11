be1b3955b7a82ca1793d2fc50453bab9d20f2a84
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-12 03:37:37 +0530

    creating for directories and adding conditions

--- file.js
+++ file.js
@@ -18,12 +18,22 @@ const fs  = require('fs');
 // });
 
 // directories
-fs.mkdir('./assets', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('folder created');
-})
+if ( !fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created');
+    })
+} else {
+    fs.rmdir('./assets', (err)=> {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder deleted');
+    })
+}
+
 
 
 
