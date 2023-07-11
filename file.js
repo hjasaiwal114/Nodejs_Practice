e3595d8ef8a88cf3488f6cd3bf5cc7accf857a3a
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-12 03:51:11 +0530

    deleting file

--- file.js
+++ file.js
@@ -35,4 +35,11 @@ const fs  = require('fs');
 // }
 
 // deleting files
-if (fs.existsSync('./docs/deleteme.txt'))
+if (fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('file deleted');
+    })
+}
