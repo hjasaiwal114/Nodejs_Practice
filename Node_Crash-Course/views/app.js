6c3852c41b5e71522d1d259e5534f85b8d4beef8
hjasaiwal114 <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-16 01:25:53 +0530

    adding title on homes

--- Node_Crash-Course/views/app.js
+++ Node_Crash-Course/views/app.js
@@ -10,19 +10,19 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
-    res.redirect('/about');
+    res.redirect('/about', {title: 'About'});
 });
 
 app.get('/blogs/create',(req, res)=>{
-    res.render();
+    res.render('create', {title: 'Creat a new Blog' });
 })
 
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404'); 
+    res.status(404).render('404', { title: '404'}); 
 });
